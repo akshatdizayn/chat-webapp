c21b2a918233ca01ac01dd16c867f37b466f64ad
akshat-dizaynio <akshat@dizayn.io>
2024-03-26 19:46:06 +0530

    sign in with google intialized

---
+++ chat-webapp/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBtPIk1_4GkTALQ0DP6rmJ1uLDYcdG_Gk0",
+  authDomain: "chat-webapp-100da.firebaseapp.com",
+  projectId: "chat-webapp-100da",
+  storageBucket: "chat-webapp-100da.appspot.com",
+  messagingSenderId: "757753890758",
+  appId: "1:757753890758:web:9346fd9731013ebd3494c7",
+  measurementId: "G-541QSQ8XVN",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
